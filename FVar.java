aa68d19d018cfdb7c06ba3d785c6ccdc898feac1
kanapuramarchana <126768108+kanapuramarchana@users.noreply.github.com>
2023-05-17 10:18:29 +0530

    Create FVar.java

---
+++ FVar.java
@@ -0,0 +1,10 @@
+public class FVar
+{
+     int x=10;
+    public static void main(String[] args)
+    {
+        FVar myObj = new FVar();
+        myObj.x = 25;
+        System.out.println(myObj.x);
+    }
+}
